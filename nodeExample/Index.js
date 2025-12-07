3452faf6bb705b50638a66c1c0433f65b34ea2d4
Amrutaquickitdotnet <velankaramruta@gmail.com>
2025-12-07 12:27:55 +0530

    added

--- nodeExample/Index.js
+++ nodeExample/Index.js
@@ -1,6 +1,22 @@
-function Add(a,b){
-    return a+b;
+var arr=[1,2,3,4,5,6,7,8,8,3,9];
+
+var duplicates={};
+for(let i=0;i<arr.length;i++){
+ let num=arr[i];
+
+ if(duplicates[num]){
+    duplicates[num]=duplicates[num]+1;
+ }else {
+    duplicates[num]=1;
+ }
+
 }
 
+console.table(duplicates);
 
-console.log(Add(2,3));
+let arrKey=Object.keys(duplicates);
+for(var i=0; i<arrKey.length;i++){
+    if(duplicates[arrKey[i]]>1){
+        console.log(arrKey[i],duplicates[arrKey[i]]);
+    }
+}
