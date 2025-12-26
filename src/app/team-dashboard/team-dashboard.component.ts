30560a329a7426cb136611acd34a63a7a9679bbf
Your name <mt.kawade81@gmail.com>
2025-12-26 16:56:05 +0530

    added

--- src/app/team-dashboard/team-dashboard.component.ts
+++ src/app/team-dashboard/team-dashboard.component.ts
@@ -26,22 +26,65 @@ export class TeamDashboardComponent {
         assignedUser: "Amit",
         taskDetail: "As a process we need to send an appointment letter to newly joined resource.",
         assignedBy: "Amit Kawade",
+        endDateTime: "31st Dec 2025",
         priority: "High"
       },
+      {
+        task: "Verification of Newly Joined Resource",
+        assignedUser: "Amit",
+        taskDetail: "Dashboard design for landing page for respective project.",
+        assignedBy: "Amit Kawade",
+        endDateTime: "31st Dec 2025",
+        priority: "High"
+      },
+
+    ]
+  }
+
+  data={
+    name: "Open",
+    count: 7,
+    list: [
       {
         task: "Send Appointment Lette",
         assignedUser: "Amit",
         taskDetail: "As a process we need to send an appointment letter to newly joined resource.",
         assignedBy: "Amit Kawade",
+        endDateTime: "31st Dec 2025",
         priority: "High"
       },
       {
-        task: "Send Appointment Lette",
+        task: "Perform Background Verification of Newly Joined Resource",
         assignedUser: "Amit",
-        taskDetail: "As a process we need to send an appointment letter to newly joined resource.",
+        taskDetail: "Dashboard design for landing page for respective project.",
         assignedBy: "Amit Kawade",
+        endDateTime: "31st Dec 2025",
         priority: "High"
-      }
+      },
+
+    ]
+  }
+
+  data={
+    name: "Completed",
+    count: 7,
+    list: [
+      {
+        task: "Follow up with seniors for incremented salary details",
+        assignedUser: "Amit",
+        taskDetail: "Dashboard design for landing page for respective project.",
+        assignedBy: "Amit Kawade",
+        endDateTime: "31st Dec 2025",
+        Status: "Completed"
+      },
+      {
+        task: "Newly Open Bank Account of Newly Joined Resource",
+        assignedUser: "Amit",
+        taskDetail: "Completed Document Process ",
+        assignedBy: "Amit Kawade",
+        endDateTime: "31st Dec 2025",
+        Status: "Completed"
+      },
 
     ]
   }
